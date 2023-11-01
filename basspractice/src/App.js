151f1021918fabd28f2945077b7cace14735af52
JoelOkuseinde <joelokuseinde456@gmail.com>
2023-11-01 22:07:54 +0000

    2nd commit, css added(unfinshed)

--- basspractice/src/App.js
+++ basspractice/src/App.js
@@ -3,24 +3,30 @@ import './App.css';
 function App() {
   return (
   <main>
+    <div className="first">
     <h1>BassPractice</h1>
     <p>Press the Button and get a key and some numbers</p>
     <form>
-    <button type="submit">Let's Go!</button>
+    <button className="go" type="submit">Let's Go!</button>
     </form>
-    <h2>C#</h2>
-    <h3>1 - 6 - 3</h3> {/* This is gonna need array destructuring to render */}
-    <p>...Alternatively, here are some songs in the key of C#</p>
+    </div>
+    <div className="result">
+    
+    <div className="key">C#</div>
+    <div className="num">1-6-3</div> {/* This is gonna need array destructuring to render */}
+    <p className="altCarousel">...Alternatively, here are some songs in the key of C#</p>
+    
     <div className="carousel">
       <div className="songDisplay">
-        <img src="" alt="album-cover" />
-        <p>Every Praise</p>
-        <p>Hezekiah Walker</p>
+        <img src="./everyPraise.png" width="250"alt="album-cover" />
+        <p className="title">Every Praise</p>
+        <p className="artist">Hezekiah Walker</p>
       </div>
       <div className="Links">
         <p>Spotify</p>
         <p>Apple Music</p>
       </div>
+    </div>
     </div>
   </main>
   );
