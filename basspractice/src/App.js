ebb94e482e7bf416511f179a3f37f8da8cf79111
JoelOkuseinde <joelokuseinde456@gmail.com>
2023-11-15 23:17:32 +0000

    update the num to be random

--- basspractice/src/App.js
+++ basspractice/src/App.js
@@ -1,20 +1,40 @@
 import './App.css';
+import {useEffect, useState} from "react";
+
 
 function App() {
+  const listOfKeys = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+  
+  const [num1,setNum1] = useState(0) 
+  const [num2,setNum2] = useState(0) 
+  const [num3,setNum3] = useState(0) 
+
+  const [key, setKey] = useState("A")
+
+  function handleClick(ev) {
+    ev.preventDefault();
+    setNum1(Math.floor(Math.random()*7) + 1)
+    setNum2(Math.floor(Math.random()*7) + 1)
+    setNum3(Math.floor(Math.random()*7) + 1)
+
+    const RandomIndex = Math.floor(Math.random()*listOfKeys.length)
+    setKey(listOfKeys[RandomIndex])
+  }
+
   return (
   <main>
     <div className="first">
     <h1>BassPractice</h1>
     <p>Press the Button and get a key and some numbers</p>
     <form>
-    <button className="go" type="submit">Let's Go!</button>
+    <button className="go" type="submit" onClick={handleClick}>Let's Go!</button>
     </form>
     </div>
     <div className="result">
     
-    <div className="key">C#</div>
-    <div className="num">1-6-3</div> {/* This is gonna need array destructuring to render */}
-    <p className="altCarousel">...Alternatively, here are some songs in the key of C#</p>
+    <div className="key">{key}</div>
+    <div className="num">{num1}-{num2}-{num3}</div> {/* This is gonna need array destructuring to render */}
+    <p className="altCarousel">...Alternatively, here are some songs in the key of {key}</p>
     
     <div className="carousel">
       <div className="songDisplay">
